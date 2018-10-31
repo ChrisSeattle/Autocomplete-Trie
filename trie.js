f4c595f210f04b0c3056cd954c533382b78d35c7
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-10-30 22:31:00 -0700

    tried for a little, but JS rusty

--- trie.js
+++ trie.js
@@ -26,10 +26,61 @@
  *   An object containing all words from given dictionary
  *   stored in a Trie structure that is compatible with check()
  */
-const create = dictionary => {
-
-  return {};
+class Node {
+  constructor(data, children=none, value=none) {
+    this.data = data; 
+    if (!children) {
+      this.children = {};
+    } 
+    this.value = value; 
+  }
 }
+
+class Trie {
+  constructor() {
+    this.root = Node('root');
+  }
+}
+
+ const create = dictionary => {
+  // takes in an object with key: value pairs of word: definition-object
+  // definition-object is a key: value of "definition": string for definition of this this word
+  // Output: return an object of Nodes
+  // each Node has a "value" set to null or an object of "definition": string for definition for this word
+  // if the value is null, this is not the end of a word. 
+  // Each Node has a "children" value set to either {} if this node is the end of a word input
+  // or set to some number of following Nodes representing letters of an input word. 
+
+  result = {"value": null, "children": {}};
+  // seperate each input word
+    for (var word in dictionary) {
+      var definition = dictionary[word];
+      curr = result.children; 
+      // insert word into our Trie
+      for (var i of word) {
+        if (curr[word[i]]) {
+          // adding to an exisiting Node
+          if (i == word.length - 1 ) {
+            prop.value = definition; 
+          }
+        } else {
+          // create a Node for this letter & input data
+          const prop = {};
+          prop.children = {};
+          if (i == word.length - 1 ) {
+            prop.value = definition; 
+          } else { 
+            prop.value = null;
+          }
+          curr[word[i]] = prop; 
+        // next letter will be nested inside current Node children
+        curr = curr[word[i]].children; 
+        }
+      }  // end looping for each letter in this word
+    } // end for looping for each input word: definition
+
+  return {result};
+}  // end const create(dictionary)
 
 /*
  * Write an algorithm that checks a trie for a query
